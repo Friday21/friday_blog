27e5527e6d144e72dc01fe733a040e774ee624d7
lidongyong <lidongyong@corp.netease.com>
2018-10-28 14:25:49 +0800

    add nginx

--- docker.sh
+++ docker.sh
@@ -2,6 +2,6 @@
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 service mysql start
 cd /friday/friday_blog
-/etc/init.d/nginx start -c nginx.conf
+/usr/sbin/nginx -c /friday/friday_blog/nginx.conf
 echo yes| python3.6 manage.py collectstatic
 gunicorn blog_django.wsgi:application --bind 0.0.0.0:8000 --workers 4
