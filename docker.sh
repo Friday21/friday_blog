1a7f34e4eecf19c8e90a7f58a8b26a9ff5ad25c3
lidongyong <lidongyong@corp.netease.com>
2018-10-28 11:45:09 +0800

    debug

--- docker.sh
+++ docker.sh
@@ -2,6 +2,6 @@
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 service mysql start
 /etc/init.d/nginx start
-cd /friday/blog_django
+cd /friday/friday_blog
 echo yes| python3.6 manage.py collectstatic
 gunicorn blog_django.wsgi:application --bind 0.0.0.0:8000 --workers 4
